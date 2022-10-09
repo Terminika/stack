2bc5e34a831463f76296fdadce69ee87799960c2
Terminika <Jkfleirb_cj_cvtnfyrjq_500>
2022-10-09 10:49:05 +0300

    basic funcs

--- main.cpp
+++ main.cpp
@@ -20,10 +20,13 @@ typedef int elem_t[];
 
 int stack_Ctor(Stack* stk, int capacity)
 {
+    printf("size ctor: %lu\n", (*stk).size);
+    printf("capacity ctor: %lu\n", (*stk).capacity);
     stk = (Stack*) calloc(capacity, sizeof(int));
     (*stk).size = 0;
     (*stk).capacity = capacity;
-    printf("%lu\n", (*stk).capacity);
+    printf("size ctor: %lu\n", (*stk).size);
+    printf("capacity ctor: %lu\n", (*stk).capacity);
     return 1;
 }
 
@@ -34,12 +37,14 @@ int stack_Dtor(Stack* stk)
 
 int Stack_Push(Stack* stk, int elem)
 {
-    printf("%lu\n", (*stk).size);
-    printf("%i %lu\n", elem, (*stk).capacity);
+    printf("size before push: %lu\n", (*stk).size);
+    printf("elem, capacity: %i %lu\n", elem, (*stk).capacity);
     if ((*stk).size == (*stk).capacity)
         return OVERLOAD;
     (*stk).data[(*stk).size] = elem;
     (*stk).size++;
+    printf("size before push: %lu\n", (*stk).size);
+    printf("elem, capacity: %i %lu\n", elem, (*stk).capacity);
     return ALL_RIGHT;
 }
 
@@ -53,6 +58,6 @@ int main()
     Stack stk = {};
     stack_Ctor(&stk, 5);
     Stack_Push(&stk, 37);
-    printf("%i\n", stk.data[0]);
+    printf("result: %i\n", stk.data[0]);
     return 1;   
 }
