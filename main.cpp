0b9fbff4400665cc0ebb70bd9b8599c074c19fae
Terminika <Jkfleirb_cj_cvtnfyrjq_500>
2022-10-11 23:34:07 +0300

    errors

--- main.cpp
+++ main.cpp
@@ -19,15 +19,15 @@ typedef struct
     // elem_t * data;
     size_t size;
     size_t capacity;
-    elem_t data[]; 
+    elem_t* data; 
 
 } Stack;
 
 int stack_Ctor(Stack* stk, int capacity)
 {
-    ((*stk).data) = calloc(capacity, sizeof(elem_t));
-    //(*stk).size = 0;
-    //(*stk).capacity = capacity;
+    (*stk).data = (elem_t*)calloc(capacity, sizeof(elem_t));
+    (*stk).size = 0;
+    (*stk).capacity = capacity;
     return 1;
 }
 
@@ -42,8 +42,12 @@ void stack_resize(Stack* stk, float step)
     {
         (*stk).capacity = 1;
     } 
-    (*stk).data = realloc(&(*stk).data, ((*stk).capacity * step) * sizeof(elem_t)); 
-    // full poison
+    long int new_capacity = ((*stk).capacity * step);
+    (*stk).data = (elem_t*)realloc((*stk).data, new_capacity * sizeof(elem_t)); 
+    for (long int i=(*stk).size; new_capacity; i++)
+    {
+        (*stk).data[i] = POISON;
+    }
 }
 
 int Stack_Push(Stack* stk, elem_t elem)
@@ -56,8 +60,8 @@ int Stack_Push(Stack* stk, elem_t elem)
     (*stk).data[(*stk).size] = elem;
     (*stk).size++;
     
-    //printf("size before push: %lu\n", (*stk).size);
-    //printf("elem, capacity: %i %lu\n", (*stk).data[0], (*stk).capacity);
+    printf("size before push: %lu\n", (*stk).size);
+    printf("elem, capacity: %Lf %lu\n", (*stk).data[0], (*stk).capacity);
     return ALL_RIGHT;
 }
 
@@ -77,22 +81,57 @@ elem_t stack_top(Stack* stk, elem_t* value)
     return top;
 }
 
-void dump()
+
+void stack_dump(Stack* stk, int st)
 {
+    char status[] = "ok";
+    printf("Stack [%lu] (%s)\n {\n", (long int)stk, status); 
+    printf("size=%lu\n", (*stk).size);
+    printf("capacity=%lu\n", (*stk).size);
+    //printf("data[%lu]\n", (long int)&(*stk).data);
+    if ((*stk).size)
+    {
+        for (long int i=0; (*stk).size; i++)
+        {
+            printf("[%li]=%Lf\n", i, (*stk).data[i]);
+        }
+        printf("}\n");
+        printf("\n\n\n");
+    }
+    else
+        printf("empty");
 
 }
 
 int main()
 {
-    Stack stk = {0, 5};
-    elem_t pop = 0;
-    stack_Ctor(&stk, 5);
+    Stack stk = {};
+    stk.size = 0;
+    stk.capacity = 5;
+    stk.data = (elem_t*)calloc(5, sizeof(elem_t));
+    /*for (long int i=stk.size; 5; i++)
+    {
+        (stk.data[i]) = 22222222222222; //(elem_t)POISON;
+    }*/
+
+    //stack_dump(&stk, 1);
+    elem_t pop = 0; 
+    //stack_Ctor(&stk, 5);
+    //stack_dump(&stk, 1);
+
     Stack_Push(&stk, 37);
+    return 0;
+    stack_dump(&stk, 1);
+
     //printf("result: %Lf\n", stk.data[0]);
     stack_pop(&stk, &pop);
+    stack_dump(&stk, 1);
+
+
     printf("pop: %Lf\n", pop);
     printf("after pop: %Lf\n", stk.data[stk.size - 1]);
     stack_resize(&stk, UP_MEM_STEP);
+    stack_dump(&stk, 1);
     return 1;   
 }
 
