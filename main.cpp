a55ec358d25233b8ce29930066c9658f9bdcdaba
Terminika <Jkfleirb_cj_cvtnfyrjq_500>
2022-10-23 23:25:34 +0300

    code style + fixes

--- main.cpp
+++ main.cpp
@@ -1,138 +1,18 @@
 #include <stdio.h>
-#include <cstdlib>
-#include <locale.h>
-#include <assert.h>
-#include <math.h>
+#include "func.h"
 
-#define UP_MEM_STEP 1.7F
-#define DOWN_MEM_STEP pow(1.5, -1)
-#define POISON 0xBADDEDBADl
-enum {
-    OVERLOAD = -1,
-    ALL_RIGHT = 1
-};
-
-typedef long double elem_t;
-
-typedef struct
-{
-    // elem_t * data;
-    size_t size;
-    size_t capacity;
-    elem_t* data; 
-
-} Stack;
-
-int stack_Ctor(Stack* stk, int capacity)
-{
-    (*stk).data = (elem_t*)calloc(capacity, sizeof(elem_t));
-    (*stk).size = 0;
-    (*stk).capacity = capacity;
-    return 1;
-}
-
-int stack_Dtor(Stack* stk)
-{
-    return 0;
-}
-
-void stack_resize(Stack* stk, float step)
-{
-    if (!(*stk).capacity)
-    {
-        (*stk).capacity = 1;
-    } 
-    long int new_capacity = ((*stk).capacity * step);
-    (*stk).data = (elem_t*)realloc((*stk).data, new_capacity * sizeof(elem_t)); 
-    for (long int i=(*stk).size; new_capacity; i++)
-    {
-        (*stk).data[i] = POISON;
-    }
-}
-
-int Stack_Push(Stack* stk, elem_t elem)
-{
-    if ((*stk).size == (*stk).capacity)
-    {
-        stack_resize(stk, UP_MEM_STEP);
-    }
-    assert(int ((*stk).size <= (*stk).capacity));
-    (*stk).data[(*stk).size] = elem;
-    (*stk).size++;
-    
-    printf("size before push: %lu\n", (*stk).size);
-    printf("elem, capacity: %Lf %lu\n", (*stk).data[0], (*stk).capacity);
-    return ALL_RIGHT;
-}
-
-void stack_pop(Stack* stk, elem_t* value)
-{   
-    *value = (*stk).data[(*stk).size - 1];
-    if ((*stk).size < (*stk).capacity / 2 && (*stk).size >= 0)
-    {
-        stack_resize(stk, DOWN_MEM_STEP);
-    }
-    (*stk).data[--(*stk).size] = POISON;
-}
-
-elem_t stack_top(Stack* stk, elem_t* value)
-{
-    elem_t top = (*stk).data[(*stk).size--];
-    return top;
-}
-
-
-void stack_dump(Stack* stk, int st)
-{
-    char status[] = "ok";
-    printf("Stack [%lu] (%s)\n {\n", (long int)stk, status); 
-    printf("size=%lu\n", (*stk).size);
-    printf("capacity=%lu\n", (*stk).size);
-    //printf("data[%lu]\n", (long int)&(*stk).data);
-    if ((*stk).size)
-    {
-        for (long int i=0; (*stk).size; i++)
-        {
-            printf("[%li]=%Lf\n", i, (*stk).data[i]);
-        }
-        printf("}\n");
-        printf("\n\n\n");
-    }
-    else
-        printf("empty");
-
-}
 
 int main()
 {
-    Stack stk = {};
-    stk.size = 0;
-    stk.capacity = 5;
-    stk.data = (elem_t*)calloc(5, sizeof(elem_t));
-    /*for (long int i=stk.size; 5; i++)
-    {
-        (stk.data[i]) = 22222222222222; //(elem_t)POISON;
-    }*/
+    Stack_t stk;
 
-    //stack_dump(&stk, 1);
-    elem_t pop = 0; 
-    //stack_Ctor(&stk, 5);
-    //stack_dump(&stk, 1);
-
-    Stack_Push(&stk, 37);
+    stack_ctor(&stk, 10);
+    stack_dump(&stk);
+    stack_push(&stk, 37);
+    stack_dump(&stk);
+    stack_dtor(&stk);
+    stack_dump(&stk);
     return 0;
-    stack_dump(&stk, 1);
-
-    //printf("result: %Lf\n", stk.data[0]);
-    stack_pop(&stk, &pop);
-    stack_dump(&stk, 1);
-
-
-    printf("pop: %Lf\n", pop);
-    printf("after pop: %Lf\n", stk.data[stk.size - 1]);
-    stack_resize(&stk, UP_MEM_STEP);
-    stack_dump(&stk, 1);
-    return 1;   
 }
 
 /*канарейки - 2 long long, выделются сразу при создании стека, в начало и конец  указатель на стек - начиная с данных
